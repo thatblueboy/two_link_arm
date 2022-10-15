484b1b7bac0d9d78e2de6ef03a6b4696f213cab8
thatblueboy <f20212485@goa.bits-pilani.ac.in>
2022-10-16 04:37:01 +0530

    centroid detection

--- scripts/CV.py
+++ scripts/CV.py
@@ -3,49 +3,65 @@
 import cv2
 import rospy
 from two_link_arm.msg import Coords
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
-# phadke chutiya
+
+
 class computerVision():
 
-    def _init_(self):
+    def __init__(self):
         rospy.init_node('CV', anonymous=True)
+        self.bridge = CvBridge()
 
-        rospy.Subscriber('camera feed', image, self.findCoords)
+        # rospy.wait_for_message('two/mybot/camera/image_raw', sensor_msgs.)
+
+        rospy.Subscriber('two/mybot/camera/image_raw', Image, self.findCoords)
         self.pub = rospy.Publisher('/centroid_coordinates', Coords, queue_size= 10)
 
         rospy.spin()
 
     def findCoords(self, img):
+        img = self.bridge.imgmsg_to_cv2(img,desired_encoding="passthrough") #converting image to cv2 format
+        img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)    #image was in RGB but cv2 reads BGR
 
-        imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert img to grayscale 
+        print(type(img))
 
-        ret, thresh1 = cv2.threshold(imgray, 20, 255, cv2.THRESH_BINARY) #get binary file of grayscale
+        # imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert img to grayscale
+        imgray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) #convert img to grayscale
+
+
+        ret, thresh1 = cv2.threshold(imgray, 225, 255, cv2.THRESH_BINARY) #get binary file of grayscale
 
         contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #find contour
 
         cv2.drawContours(img, contours, -1, (0, 255, 0), 3) #draw contours, not required
 
-        # calculate moments of binary image
-        M = cv2.moments(thresh1)    
+        # # calculate moments of binary image
+        M = cv2.moments(thresh1)
 
-        # calculate x,y coordinate of center
+        # # calculate x,y coordinate of center
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
 
-        self.publishCoords(cX, cY)
-        
-        # cv2.imshow("Image", img)
-        # cv2.waitKey() 
+        print(cX, cY)
+
+        cv2.circle(img, (cX, cY), 5, (0, 0, 255), -1)
+
+        # self.publishCoords(cX, cY)
+        # cv2.imshow("image", imgray)
+        cv2.imshow("Image", img)
+        cv2.waitKey()
 
 
 
 
-    def publishCoords(self, cX, cY):
-        # coords = [cX, cY]
-        coords = Coords()
-        coords.x = cX
-        coords.y = cY
-        self.pub.publish(coords)
+    # def publishCoords(self, cX, cY):
+    #     # coords = [cX, cY]
+    #     coords = Coords()
+    #     coords.x = cX
+    #     coords.y = cY
+    #     self.pub.publish(coords)
 
 
 if __name__ == '__main__':
